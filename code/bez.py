1f4edba15ffa6da4b7198f68d89dc2a5b1ad3a49
Simon Abrelat <simon.abrelat@gmail.com>
2018-10-30 21:52:13 -0400

    Done Bezier Draft

--- code/bez.py
+++ code/bez.py
@@ -3,27 +3,27 @@ from scipy.special import binom  # binomial coefficient
 import matplotlib.pyplot as plt  # plotting
 from math import pow # power function
 
-cpx: [float] = [1.0, 3.0, 6.0, 8.0]
-cpy: [float] = [1.0, 9.0, 6.0, 1.0]
+cpx: [float] = [0.0, 0.5, 2.0]
+cpy: [float] = [0.0, 3.0, 0.0]
 
 t = lin(0, 1, 100)  # Input as 100 floats between 0 and 1
-num: int = 3  # The degree
+num: int = 2  # The degree 0 indexed
 
 # returns the value from the Bernstein basis function at the point x
 def bbf(i: int, n: int, x: float) -> float:
     return binom(n, i) * pow(x, i) * pow((1 - x), (n-i))
 
-# De Cas
+# Generalized De Casteljau's Explicit formula 
 def f(a: [float], t: float) -> float:
     ret = 0
     for i in range(num + 1):
         ret += a[i] * bbf(i, num, t)
     return ret
 
-def x(t: float) -> float: return f(cpx, t)
-def y(t: float) -> float: return f(cpy, t)
+def x(t: float) -> float: return f(cpx, t) # implemented on X's control points
+def y(t: float) -> float: return f(cpy, t) # implemented on Y's control points
 
-plt.plot([x(i) for i in t], [y(i) for i in t])
-plt.plot(cpx, cpy, 'r--')
-plt.plot(cpx, cpy, 'ro')
+plt.plot([x(i) for i in t], [y(i) for i in t]) # plots X and Y
+plt.plot(cpx, cpy, 'ro') # plots the control points
+plt.plot(cpx, cpy, 'r:') # plots lines between control points
 plt.show()
